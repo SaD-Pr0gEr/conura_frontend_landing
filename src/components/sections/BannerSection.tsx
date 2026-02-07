9428c5957fdeeb7bec9cfca3760a3b50f7b00d47
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2026-02-07 21:14:22 +0500

    home banners

--- src/components/sections/BannerSection.tsx
+++ src/components/sections/BannerSection.tsx
@@ -10,18 +10,18 @@ const slides = [
         theme: 'light',
         buttonClass: 'btn btn-primary banner-cta',
     },
-    // {
-    //     id: 2,
-    //     title: 'IT решения: сайты, боты, интеграции',
-    //     theme: 'dark',
-    //     buttonClass: 'btn btn-white banner-cta',
-    // },
-    // {
-    //     id: 3,
-    //     title: 'Контекстная реклама: от А до Я',
-    //     theme: 'dark',
-    //     buttonClass: 'btn btn-primary banner-cta',
-    // },
+    {
+        id: 2,
+        title: 'IT решения: сайты, боты, интеграции',
+        theme: 'dark',
+        buttonClass: 'btn btn-white banner-cta',
+    },
+    {
+        id: 3,
+        title: 'Контекстная реклама: от А до Я',
+        theme: 'dark',
+        buttonClass: 'btn btn-primary banner-cta',
+    },
 ];
 
 export function BannerSection() {
@@ -65,45 +65,55 @@ export function BannerSection() {
                         className={`banner-1 banner-slide banner-slide-${slides[0].theme} ${0 === currentSlide ? 'active' : ''}`}
                     >
                         <div className="banner-img">
-                            <img className="left-top-fragment" src="/images/home/banner_1_left_top.svg" alt="left top fragment"/>
-                            <img className="lightning-fragment" src="/images/home/lighting.svg" alt="light top fragment"/>
-                            <img className="a-logo-fragment" src="/images/a-logo.svg" alt="a logo fragment"/>
-                            <img className="it-logo-fragment" src="/images/it-park-logo.svg" alt="it park logo"/>
-                            <img className="human-ufo-fragment" src="/images/human-and-ufo.png" alt="human and ufo fragment"/>
+                            <img className="left-top-fragment" src="/images/home/banner_1_left_top.svg"
+                                 alt="left top fragment"/>
+                            <img className="lightning-fragment" src="/images/home/lighting.svg"
+                                 alt="light top fragment"/>
+                            <img className="a-logo-fragment scalable-img" src="/images/a-logo.svg"
+                                 alt="a logo fragment"/>
+                            <img className="it-logo-fragment scalable-img" src="/images/it-park-logo.svg"
+                                 alt="it park logo"/>
+                            <img className="human-ufo-fragment scalable-img" src="/images/human-and-ufo.png"
+                                 alt="human and ufo fragment"/>
                         </div>
                         <div className="banner-content">
                             <h1 className="banner-title">{slides[0].title}</h1>
                             <button onClick={openModal} className={slides[0].buttonClass}>Оставить заявку</button>
                         </div>
                     </div>
-                    {/*<div*/}
-                    {/*    className={`banner-slide banner-slide-${slides[1].theme} ${1 === currentSlide ? 'active' : ''}`}*/}
-                    {/*>*/}
-                    {/*    <img*/}
-                    {/*        alt={slides[1].title}*/}
-                    {/*        className="banner-slide-image"*/}
-                    {/*        loading='lazy'*/}
-                    {/*        decoding='async'*/}
-                    {/*    />*/}
-                    {/*    <div className="banner-content">*/}
-                    {/*        <h1 className="banner-title">{slides[1].title}</h1>*/}
-                    {/*        <button onClick={openModal} className={slides[1].buttonClass}>Оставить заявку</button>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
-                    {/*<div*/}
-                    {/*    className={`banner-slide banner-slide-${slides[2].theme} ${2 === currentSlide ? 'active' : ''}`}*/}
-                    {/*>*/}
-                    {/*    <img*/}
-                    {/*        alt={slides[2].title}*/}
-                    {/*        className="banner-slide-image"*/}
-                    {/*        loading='lazy'*/}
-                    {/*        decoding='async'*/}
-                    {/*    />*/}
-                    {/*    <div className="banner-content">*/}
-                    {/*        <h1 className="banner-title">{slides[2].title}</h1>*/}
-                    {/*        <button onClick={openModal} className={slides[2].buttonClass}>Оставить заявку</button>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div
+                        className={`banner-2 banner-slide banner-slide-${slides[1].theme} ${1 === currentSlide ? 'active' : ''}`}
+                        style={{backgroundImage: "url('/images/home/white-stars-bg.png')"}}
+                    >
+                        <div className="banner-img">
+                            <img className="left-stars-top" src="/images/home/stars.png" alt="stars"/>
+                            <img className="center-cloud scalable-img" src="/images/home/clouds.svg" alt="clouds"/>
+                            <img className="ufo-robot scalable-img" src="/images/home/robot.png" alt="robot"/>
+                            <img className="cosmo" src="/images/home/cosmo.png" alt="cosmo"/>
+                            <img className="comet scalable-img" src="/images/home/comet.svg" alt="comet"/>
+                            <img className="web scalable-img" src="/images/home/web.png" alt="web"/>
+                        </div>
+                        <div className="banner-content">
+                            <h1 className="banner-title">{slides[1].title}</h1>
+                            <button onClick={openModal} className={slides[1].buttonClass}>Оставить заявку</button>
+                        </div>
+                    </div>
+                    <div
+                        className={`banner-3 banner-slide banner-slide-${slides[2].theme} ${2 === currentSlide ? 'active' : ''}`}
+                    >
+                        <div className="banner-img">
+                            <img className="banner_3_left_top" src="/images/home/banner_3_left_top.svg" alt="banner 3 left top"/>
+                            <img className="banner_3_a_logo" src="/images/a-logo.svg" alt="banner 3 a logo"/>
+                            <img className="banner_3_analytics" src="/images/home/analytics.svg" alt="banner 3 analytics"/>
+                            <img className="banner_3_ufoman" src="/images/home/light-ufoman.png" alt="banner 3 ufoman"/>
+                            <img className="banner_3_yellow_arrow" src="/images/yellow-arrow-logo.svg" alt="banner 3 yellow arrow"/>
+                            <img className="banner_3_square" src="/images/home/square.svg" alt="banner 3 square"/>
+                        </div>
+                        <div className="banner-content">
+                            <h1 className="banner-title">{slides[2].title}</h1>
+                            <button onClick={openModal} className={slides[2].buttonClass}>Оставить заявку</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -124,7 +134,7 @@ export function BannerSection() {
 
                 {/* Arrows */}
                 <div className="banner-arrows">
-                <button className="banner-arrow banner-arrow-prev" aria-label="Предыдущий слайд"
+                    <button className="banner-arrow banner-arrow-prev" aria-label="Предыдущий слайд"
                             onClick={prevSlide}>
                         <svg width="10" height="16" viewBox="0 0 10 16" fill="none">
                             <path d="M8 2L2 8L8 14" stroke="#101010" strokeWidth="2" strokeLinecap="round"
