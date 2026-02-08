c56b482223386f3973546d0ee0ef495b5bbf74c2
SaD-Pr0gEr <sad.pr0ger@gmail.com>
2026-02-09 01:14:22 +0500

    how we work

--- src/components/sections/HowWeWorkSection.tsx
+++ src/components/sections/HowWeWorkSection.tsx
@@ -7,37 +7,37 @@ const steps = [
         id: 0,
         title: 'Консультация',
         description: 'Обсуждаем задачу, определяем цели и возможные направления решения',
-        icon: 'https://www.figma.com/api/mcp/asset/caabbf35-ea7c-4c67-95f9-2d501490e28f',
+        icon: '/images/home/hww_1_section.png',
     },
     {
         id: 1,
         title: 'Заключение договора',
         description: 'Фиксируем объём работ, сроки и ответственность сторон',
-        icon: 'https://www.figma.com/api/mcp/asset/aa89f240-ae68-4875-b8cb-f0d384ccfe0c',
+        icon: '/images/home/hww_2_section.png',
     },
     {
         id: 2,
         title: 'Анализ и стратегия',
         description: 'Изучаем исходные данные, формируем план действий и подход к реализации',
-        icon: 'https://www.figma.com/api/mcp/asset/eb43cf01-63f5-4607-889d-08807bbe4619',
+        icon: '/images/home/hww_3_section.png',
     },
     {
         id: 3,
         title: 'Реализация проекта',
         description: 'От технической части до дизайна и контента',
-        icon: 'https://www.figma.com/api/mcp/asset/06b978d5-1644-4d79-9279-64cc84a6b6c9',
+        icon: '/images/home/hww_4_section.png',
     },
     {
         id: 4,
         title: 'Запуск и внедрение',
         description: 'Вводим решение в работу, обеспечиваем корректный старт и первые результаты',
-        icon: 'https://www.figma.com/api/mcp/asset/c60ccfbc-e3ab-4cc4-a9dc-dd84e80931c1',
+        icon: '/images/home/hww_5_section.png',
     },
     {
         id: 5,
         title: 'Поддержка',
         description: 'Сопровождаем проект, контролируем эффективность и вносим улучшения.',
-        icon: 'https://www.figma.com/api/mcp/asset/0c814c64-df6b-44a9-b44c-6ed2c02b6625',
+        icon: '/images/home/hww_6_section.png',
     },
 ];
 
